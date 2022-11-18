b1605c4a494c6f445f40da1350a7d9aad8d1987b
Ziad Mostafa <ziad.mostafa@apex.ai>
2022-11-18 10:50:41 +0100

    add solution for errors3

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -6,9 +6,9 @@
 
 // I AM NOT DONE
 
-use std::num::ParseIntError;
+use std::{num::ParseIntError, error::Error};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -20,6 +20,8 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
