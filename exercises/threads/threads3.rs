9bf81da35e968187165d7afb862ee6de5b88e3f6
Ziad Mostafa <ziad.mostafa@apex.ai>
2023-01-31 11:32:14 +0100

    add solutions to smart pointers and threading excersizes
    Signed-off-by: Ziad Mostafa <ziad.mostafa@apex.ai>

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -29,10 +29,12 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
 
+    let thread_tx1 = tx.clone();
+    let thread_tx2 = tx.clone();
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            thread_tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -40,7 +42,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            thread_tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
